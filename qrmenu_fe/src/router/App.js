dc3fa423eb9a733e74e33a4b04287bbe4d18ad03
karma007 <abhishekpandey.kr@gmail.com>
2022-03-23 09:39:12 +0530

    toaster added

--- qrmenu_fe/src/router/App.js
+++ qrmenu_fe/src/router/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
+import { ToastContainer } from "react-toastify";
 
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 

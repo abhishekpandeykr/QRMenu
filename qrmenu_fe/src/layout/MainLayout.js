0755936913a2b3e5e8e969f1a5be6dc8dbf24ecf
karma007 <abhishekpandey.kr@gmail.com>
2022-03-25 10:33:48 +0530

    login logout handling

--- qrmenu_fe/src/layout/MainLayout.js
+++ qrmenu_fe/src/layout/MainLayout.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
 
 const MainLayout = ({ children }) => {
   let navigate = useNavigate();
+  const auth = useContext(AuthContext);
 
   const signIn = () => {
     navigate("/login");
   };
 
+  const onSignOut = () => {
+    auth.signOut();
+  };
+
   const gotoPlaces = () => {
     navigate("/places");
   };
+
+  const getLoginOrLogoutButton = (btnText, clickHandler) => (
+    <Nav className="flex-grow-1 justify-content-end">
+      <Nav.Link onClick={clickHandler}>{btnText}</Nav.Link>
+    </Nav>
+  );
 
   return (
     <>
@@ -20,9 +32,9 @@ const MainLayout = ({ children }) => {
         <Nav>
           <Nav.Link onClick={gotoPlaces}>Places</Nav.Link>
         </Nav>
-        <Nav className="flex-grow-1 justify-content-end">
-          <Nav.Link onClick={signIn}>Login</Nav.Link>
-        </Nav>
+        {auth.token
+          ? getLoginOrLogoutButton("Sign Out", onSignOut)
+          : getLoginOrLogoutButton("Sign In", signIn)}
       </Navbar>
       <Container>{children}</Container>
     </>
